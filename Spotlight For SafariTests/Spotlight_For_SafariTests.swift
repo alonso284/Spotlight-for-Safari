a6afc6dd47c6c8607e5e0010f30eea1c89359c19
Alonso Huerta <alonso12_284@outlook.com>
2024-12-09 15:38:39 -0600

    Initial Commit

---
+++ Spotlight For SafariTests/Spotlight_For_SafariTests.swift
@@ -0,0 +1,17 @@
+//
+//  Spotlight_For_SafariTests.swift
+//  Spotlight For SafariTests
+//
+//  Created by Alonso Huerta on 09/12/24.
+//
+
+import Testing
+@testable import Spotlight_For_Safari
+
+struct Spotlight_For_SafariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
